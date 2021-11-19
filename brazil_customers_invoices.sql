0cc7f7c2723f66ddb805cf97a6179a82fcfd2feb
Stefan Borghesani <stefan.borghesani@gmail.com>
2021-11-19 10:02:35 -0600

    brazil customers invoices

---
+++ brazil_customers_invoices.sql
@@ -0,0 +1,5 @@
+SELECT FirstName, LastName, InvoiceId, InvoiceDate, BillingCountry
+FROM Invoice
+INNER JOIN Customer 
+    on Invoice.CustomerId = Customer.CustomerId
+WHERE BillingCountry like "brazil";
